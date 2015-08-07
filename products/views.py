afc66030aaf152de2796c647643917cccffcee1c
Artem Strukov <winner1040@gmail.com>
2015-08-07 10:17:49 +0300

    correct models

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from .models import Type
+
+
+def index(request):
+    type_product = Type.objects.all()
+    context = {'type_product':type_product}
+    return render(request, 'tobaccostore/index.html', context)
