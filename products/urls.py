eadd08025d587302e2068ff5504b981f9842654e
Artem Strukov <winner1040@gmail.com>
2015-08-10 16:07:17 +0300

    10/08/15

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^tobacco/$', views.tobaccolist),
+    #url(r'^shisha/$', views.shisha_list),
+    #url(r'^other/$', views.other_list),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,19 @@
+
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Type
+from .models import Product
+
 
 
 def index(request):
     type_product = Type.objects.all()
-    context = {'type_product':type_product}
+    context = {'type_product': type_product}
     return render(request, 'products/index.html', context)
+
+def tobaccolist(request):
+    product_tobacco = Product.objects.filter(type = 2)
+    context = {'product_tobacco': product_tobacco}
+    return render(request, 'products/tobacco.html', context)
