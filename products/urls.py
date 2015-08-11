e68c2717b4e2197934ca5e7473d587e684081908
Artem Strukov <winner1040@gmail.com>
2015-08-11 10:11:54 +0300

    11.08.15

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^tobacco/$', views.tobaccolist),
-    url(r'^shisha/$', views.shisha_list),
-    url(r'^other/$', views.other_list),
+    url(r'^tobacco/$', views.tobaccolist, name = 'tobacco'),
+    url(r'^shisha/$', views.shisha_list, name = 'shisha'),
+    url(r'^other/$', views.other_list, name = 'other'),
 ]
