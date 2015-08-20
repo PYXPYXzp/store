ad96af328cae28e987089cafdc4a867d01116f7b
Artem Strukov <winner1040@gmail.com>
2015-08-20 16:54:06 +0300

    upgrade templates list.html

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^tobacco/$', views.tobacco_company, name = 'tobacco_views'),
-    url(r'^tobacco/(?P<comp_id>[0-9]+)/$', views.flower_list, name = 'flowers'),
+    url(r'^tobacco/(?P<comp_id>[0-9]+)/$', views.list, name = 'flowers'),
+    #url(r'^tobacco/(?P<comp_id>[0-9]+)/detail/$', views.
     url(r'^shisha/$', views.shisha_company, name = 'shisha'),
-    url(r'^shisha/(?P<comp_id>[0-9]+)/$', views.shisha_list, name = 'shisha'),
+    url(r'^shisha/(?P<comp_id>[0-9]+)/$', views.list, name = 'shisha'),
+    url(r'^shisha/(?P<comp_id>[0-9]+)/(\d+)/$', views.detail),
     url(r'^other/$', views.other_company, name = 'other'),
-    url(r'^other/(?P<comp_id>[0-9]+)/$', views.other_list, name = 'other'),
+    url(r'^other/(?P<comp_id>[0-9]+)/detail/$', views.list, name = 'other'),
 ]
--- products/views.py
+++ products/views.py
@@ -15,27 +15,20 @@ def tobacco_company(request):
     context = {'companytob': companytob}
     return render(request, 'products/comptobaklist.html', context)
 
-def flower_list(request, comp_id):
-    flower_tobacco = Product.objects.filter(company__pk=comp_id)
-    context = {'flower_tobacco': flower_tobacco}
-    return render(request, 'products/flowerlist.html', context)
-
 def shisha_company(request):
     product_shisha = Company.objects.filter(product__type__type_product='Кальян').distinct()
     context = {'product_shisha': product_shisha}
     return render(request, 'products/compshishalist.html', context)
-
-def shisha_list(request, comp_id):
-    model_shisha = Product.objects.filter(company__pk=comp_id)
-    context = {'model_shisha': model_shisha}
-    return render(request, 'products/shishalist.html', context)
 
 def other_company(request):
     product_other = Company.objects.filter(product__type__type_product='Разное').distinct()
     context = {'product_other': product_other}
     return render(request, 'products/compotherlist.html', context)
 
-def other_list(request, comp_id):
-    model_other = Product.objects.filter(company__pk=comp_id)
-    context = {'model_other': model_other}
-    return render(request, 'products/otherlist.html', context)
+def list(request, comp_id):
+    model = Product.objects.filter(company__pk=comp_id)
+    context = {'model': model}
+    return render(request, 'products/list.html', context)
+
+def detail(request, comp_id, offset ):
+    print "bdfb"
