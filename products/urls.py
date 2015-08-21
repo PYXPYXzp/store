b4d277e3a63e92306c6fd23336f7418d1270b31d
Artem Strukov <winner1040@gmail.com>
2015-08-21 16:46:06 +0300

    21.08.15 update URL

--- products/urls.py
+++ products/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^tobacco/$', views.tobacco_company, name = 'tobacco_views'),
-    url(r'^tobacco/(?P<comp_id>[0-9]+)/$', views.list, name = 'flowers'),
-    #url(r'^tobacco/(?P<comp_id>[0-9]+)/detail/$', views.
-    url(r'^shisha/$', views.shisha_company, name = 'shisha'),
-    url(r'^shisha/(?P<comp_id>[0-9]+)/$', views.list, name = 'shisha'),
-    url(r'^shisha/(?P<comp_id>[0-9]+)/(\d+)/$', views.detail),
-    url(r'^other/$', views.other_company, name = 'other'),
-    url(r'^other/(?P<comp_id>[0-9]+)/detail/$', views.list, name = 'other'),
+    url(r'^tobacco/$', views.tobacco_company, name = 'tobacco_company'),
+    url(r'^tobacco/(?P<comp_id>\d+)/$', views.list, name = 'list'),
+    url(r'^tobacco/(?P<comp_id>\d+)/$', views.detail, name = 'detail'),
+    url(r'^shisha/$', views.shisha_company, name = 'shisha_company'),
+    url(r'^shisha/(?P<comp_id>[0-9]+)/$', views.list, name = 'list'),
+    url(r'^other/$', views.other_company, name = 'other_company'),
+    url(r'^other/(?P<comp_id>[0-9]+)/$', views.list, name = 'list'),
+
 ]
